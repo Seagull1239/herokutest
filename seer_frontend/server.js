220b8cbc1a290634c490e8b06a10ec192dae9bdf
Karran Singh <53281700+Seagull1239@users.noreply.github.com>
2020-10-25 14:37:48 +1300

    yeet

--- seer_frontend/server.js
+++
@@ -1,8 +0,0 @@
-app.use(express.static('./dist/seer-front.json'));
-
-app.get('/', function(req,res) {
-    res.sendFile('index.html', {root: 'dist/seer-front.json/'}
-);
-});
-
-app.listen(process.env.PORT || 8081);
